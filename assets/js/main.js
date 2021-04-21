b4ef70792ec97cb4b076f77490303fa49063c828
ricardo <rarrosr@gmail.com>
2021-04-21 01:36:25 -0400

    Start portfolio project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,119 @@
 /*===== SHOW MENU =====*/
 
+const showMenu = (toggleId, navId) => {
+  
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+  
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show-menu');
+    });
+  }
+};
+
+showMenu('nav-toggle', 'nav-menu');
+
 
 /*===== REMOVE MENU MOBILE =====*/
+
+// 1
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach((link) => link.addEventListener('click', linkAction));
+
+// 2
+// window.addEventListener('click', function (event) {
+//   //
+//   const navMenu = document.getElementById('nav-menu');
+
+//   if (event.target != navMenu && event.target.parentNode != navMenu) {
+//     navMenu.classList.remove('show-menu');
+//   } else {
+//     navMenu.classList.add('show-menu');
+//   }
+// });
 
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
 
+//
+const sections = document.querySelectorAll('section[id]');
 
-/*===== CHANGE BACKGROUND HEADER =====*/ 
+//
+function scrollActive() {
+  
+  const scrollY = window.pageYOffset;
+
+  
+  sections.forEach((current) => {
+    
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    
+    sectionId = current.getAttribute('id');
+    
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+
+window.addEventListener('scroll', scrollActive);
 
 
-/*===== SHOW SCROLL TOP =====*/ 
+/*===== CHANGE BACKGROUND HEADER =====*/
+
+//
+function scrollHeader() {
+  //
+  const header = document.getElementById('header');
+
+  //
+  if (this.scrollY >= 200) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header');
+  }
+}
+
+window.addEventListener('scroll', scrollHeader);
 
 
-/*===== MIXITUP FILTER PORTFOLIO =====*/ 
+/*===== SHOW SCROLL TOP =====*/
 
-/* Link active portfolio */ 
+//
+function scrollTop() {
+  //
+  const scrollTop = document.getElementById('scroll-top');
+
+  //
+  if (this.scrollY >= 560) {
+    scrollTop.classList.add('scroll');
+  } else {
+    scrollTop.classList.remove('scroll');
+  }
+}
+
+window.addEventListener('scroll', scrollTop);
 
 
-/*===== SWIPER CAROUSEL =====*/ 
+/*===== MIXITUP FILTER PORTFOLIO =====*/
 
+/* Link active portfolio */
 
-/*===== GSAP ANIMATION =====*/ 
+/*===== SWIPER CAROUSEL =====*/
 
-
+/*===== GSAP ANIMATION =====*/
